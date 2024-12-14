74438bcb2ccd32dac22a9edafb211c77e78841b3
Sykotes <sykotes@icloud.com>
2024-12-14 17:47:50 +0000

    fixed alsa errors and updated package list in postcreate

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,20 +4,30 @@
 echo "Updating package index..."
 sudo apk update
 
-# List of packages you want to install
+# List of packages to install
 PACKAGES=(
     python3-tkinter
+    py3-virtualenv
     sdl2-dev
+    sdl2_image-dev
+    sdl2_mixer-dev
+    sdl2_ttf-dev
     mesa
     mesa-dri-gallium
     mesa-gl
     mesa-egl
     libx11
     libxext
+    cmake
+    portmidi-dev
 )
 
-# Install all the packages
 echo "Installing packages..."
 sudo apk add --no-cache "${PACKAGES[@]}"
+
+echo "Setting up python"
+virtualenv .venv
+source ".venv/bin/activate"
+pip install -r requirements.txt
 
 echo "Postcreate complete!"
